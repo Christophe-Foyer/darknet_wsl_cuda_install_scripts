71bb5b370addda9d71363dac0cca7f3b8064980b
Christophe Foyer <christophe.aa.foyer@gmail.com>
2021-03-10 17:39:55 +0000

    Update Darknet_WSL_CUDA.sh

--- Darknet_WSL_CUDA.sh
+++ Darknet_WSL_CUDA.sh
@@ -4,17 +4,19 @@ echo "This script assumes the wsl container is set up for CUDA"
 echo "You can download the CUDNN Package at: https://developer.nvidia.com/cudnn"
 echo "Check the variables at the top of this file before running (especially CUDA_ARCH_BIN, see https://en.wikipedia.org/wiki/CUDA for the version supported by your GPU)"
 
-CUDA_ARCH_BIN="5.0"
-cuda_version="11-2"
-cudnn_file="libcudnn8.deb"
-
 # TODO: replace the CUDA_ARCH_BIN in the opencv script based on a variable here
 
 [[ "$(read -e -p 'Continue? [y/N]> '; echo $REPLY)" == [Yy]* ]] && echo Continuing || (exit 1 && echo Stopping)
 
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 
-sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+CUDA_ARCH_BIN="5.0"
+ubuntu_version="ubuntu2004"
+cuda_version="11-2"
+cudnn_file="libcudnn8.deb"
+
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/${ubuntu_version}/x86_64/7fa2af80.pub
+
+sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/${ubuntu_version}/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
 
 sudo apt-get update
 
