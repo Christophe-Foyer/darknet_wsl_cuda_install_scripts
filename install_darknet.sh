a3ea381e8684cea34a0f0616ccf7a60888a50013
Christophe Foyer <christophe.aa.foyer@gmail.com>
2021-03-10 23:06:36 +0000

    Update install_darknet.sh

--- install_darknet.sh
+++ install_darknet.sh
@@ -15,4 +15,5 @@ export DYLD_LIBRARY_PATH=/usr/local/cuda/lib:/usr/local/cuda/extras/CUPTI/lib
 export LD_LIBRARY_PATH=$DYLD_LIBRARY_PATH
 export PATH=$DYLD_LIBRARY_PATH:$PATH
 
+sudo apt-get install libopencv-dev
 make
