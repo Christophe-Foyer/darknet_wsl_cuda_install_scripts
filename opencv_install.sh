6918fc71766f473975ccc623afe5e14251494d9f
Christophe Foyer <christophe.aa.foyer@gmail.com>
2021-03-10 19:26:23 +0000

    Update opencv_install.sh

--- opencv_install.sh
+++ opencv_install.sh
@@ -22,7 +22,7 @@ sudo apt-get update
 sudo apt-get install zip
 sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt-get install -y python3.6-dev python-dev python-numpy python3-numpy #python2.7-dev
+#sudo apt-get install -y python3.6-dev python-dev python-numpy python3-numpy #python2.7-dev
 sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
 sudo apt-get install -y curl
